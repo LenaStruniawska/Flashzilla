274be4729d21439b2d1646f245175e5b6aa12a9c
Magdalena Struniawska <mag.sok@onet.pl>
2022-05-16 12:23:13 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Slawek on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
